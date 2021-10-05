4be8bd538ec6833b269f3632d807fcc22ef03eea
LazyJazz <zijian.lv@hotmail.com>
2021-10-06 04:23:30 +0800

    Create Geometry Part

---
+++ Grassland/include/Geometry/GeometryDecl.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "../Math/Math.h"
+
+namespace Grassland
+{
+	namespace Geometry
+	{
+		template<typename T> struct Ray;
+
+		template<typename T>
+		class Geometry
+		{
+		public:
+			;
+		};
+	}
+}
