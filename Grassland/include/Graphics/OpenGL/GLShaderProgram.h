e8ec4c642c6e1b5bdb2e2f9a9087c9a92163c87c
LazyJazz <zijian.lv@hotmail.com>
2021-11-08 10:49:38 +0800

    Normal Update

--- Grassland/include/Graphics/OpenGL/GLShaderProgram.h
+++ Grassland/include/Graphics/OpenGL/GLShaderProgram.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "GLHeader.h"
+#include <map>
+#include <string>
 
 namespace Grassland
 {
@@ -20,9 +22,10 @@ namespace Grassland
 			public:
 				Shader();
 				Shader(const char * shader_file_path, GRL_OPENGL_SHADER_TYPE shader_type);
-				GRL_RESULT LoadShaderFromFile(const char* shader_file_path, GRL_OPENGL_SHADER_TYPE shader_type);
+				GRL_RESULT CompileShaderFromFile(const char* shader_file_path, GRL_OPENGL_SHADER_TYPE shader_type);
 				GRL_OPENGL_SHADER_TYPE GetShaderType() const;
 				uint32_t GetShaderHandle() const;
+				uint32_t Clear();
 			private:
 				uint32_t __shader_handle;
 				GRL_OPENGL_SHADER_TYPE __shader_type;
@@ -30,7 +33,34 @@ namespace Grassland
 
 			class Program
 			{
-				;
+			public:
+				Program();
+				~Program();
+				GRL_RESULT LinkShaders(
+					Shader* vertex_shader,
+					Shader* fragment_shader,
+					Shader* geometry_shader = NULL
+				);
+				void Clear();
+				int32_t GetUniformLocation(const char* uniformName);
+				void Use() const;
+				void SetFloat(int32_t location, float fvalue);
+				void SetVec2(int32_t location, const Math::Vec2& vec);
+				void SetVec3(int32_t location, const Math::Vec3& vec);
+				void SetVec4(int32_t location, const Math::Vec4& vec);
+				void SetMat2(int32_t location, const Math::Mat2& mat);
+				void SetMat3(int32_t location, const Math::Mat3& mat);
+				void SetMat4(int32_t location, const Math::Mat4& mat);
+				void SetFloat(const char* uname, float fvalue);
+				void SetVec2(const char* uname, const Math::Vec2& vec);
+				void SetVec3(const char* uname, const Math::Vec3& vec);
+				void SetVec4(const char* uname, const Math::Vec4& vec);
+				void SetMat2(const char* uname, const Math::Mat2& mat);
+				void SetMat3(const char* uname, const Math::Mat3& mat);
+				void SetMat4(const char* uname, const Math::Mat4& mat);
+			private:
+				uint32_t __program_handle;
+				std::map<std::string, int32_t> __uniform_location_map;
 			};
 		}
 	}
